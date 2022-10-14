98fa752e525221823291e086e426857a7a28a5e0
Je Studios <efjayadi@gmail.com>
2022-10-15 00:55:04 +0700

    loader fix

--- html/assets/js/geser.js
+++ html/assets/js/geser.js
@@ -28,7 +28,7 @@ slider.addEventListener('mousemove', (e) => {
 
 // preloader
 $('#status').fadeOut(); // will first fade out the loading animation
-$('#preloader').delay(550).fadeOut('slow'); // will fade out the white DIV that covers the website.
+$('#preloaderna').delay(550).fadeOut('slow'); // will fade out the white DIV that covers the website.
 $('body').delay(550).css({
     'overflow': 'visible'
 });
